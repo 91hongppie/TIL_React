ad9ea78b1c724b84a9072c6509ac26f906660d7a
91hongppie <91hongppie@gmail.com>
2021-08-25 17:34:21 +0900

    TIL React component&UI 210825

--- blog/src/App.js
+++ blog/src/App.js
@@ -8,12 +8,20 @@ function App() {
 
   let posts = '강남 고기 맛집';
   let [글제목, 글제목변경] = useState(['남자 코트 추천', '강남 맛집 추천', '재밌는 영화 추천']);
-  let [따봉, 따봉변경] = useState(0);
+  let [따봉, 따봉변경] = useState([0, 0, 0]);
+  let [modal, modal변경] = useState(false)
+  var ar = [2, 3, 4]
 
   function 제목바꾸기() {
     let newArray = [...글제목];
     newArray[0] = '여자 코트 추천'
     글제목변경(newArray)
+  }
+
+  function 따봉바꾸기(idx) {
+    let newArray = [...따봉]
+    newArray[idx] += 1
+    따봉변경(newArray)
   }
 
   return (
@@ -22,23 +30,34 @@ function App() {
         <div>개발 Blog</div>
       </div>
       <button onClick={ 제목바꾸기 }>버튼</button>
-      <div className="list">
-        <h3>{ 글제목[0] } <span onClick={ () => { 따봉변경(따봉+1) }}>❤️</span> { 따봉 } </h3>
-        <p>2월 17일 발행</p>
-        <hr/> 
-      </div>
-      <div className="list">
-        <h3>{ 글제목[1] }</h3>
-        <p>2월 18일 발행</p>
-        <hr/> 
-      </div>
-      <div className="list">
-        <h3>{ 글제목[2] }</h3>
-        <p>2월 19일 발행</p>
-        <hr/> 
-      </div>
+      {
+        글제목.map((value, idx) => {
+          return ( <div className="list">
+              <h3>{ value } <span onClick={ () => { 따봉바꾸기(idx) } }>하트</span>{ 따봉[idx] }</h3>
+              <p>2월 {17 + idx}일 발행</p>
+              <hr/>
+            </div>
+          )
+        })
+      }
+      <button onClick={ () => {modal변경(modal ? false : true)} }>버튼</button>
+      {
+        modal === true
+        ? <Modal />
+        : null
+      }
     </div>
   );
+}
+
+function Modal() {
+  return (
+    <div className="modal">
+      <h2>제목</h2>
+      <p>날짜</p>
+      <p>상세 내용</p>
+    </div>
+  )
 }
 
 export default App;
