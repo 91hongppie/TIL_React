5654c8afb5ad85fb0bf728a95461baf36dc70d64
91hongppie <91hongppie@gmail.com>
2021-12-22 12:46:23 +0900

    211222 makeShoppingMall

--- blog/src/App.js
+++ blog/src/App.js
@@ -4,14 +4,39 @@ import './App.css';
 function App() {
 
   const [글제목, 글제목변경] = useState(['남자코트 추천', '강남 우동맛집', '파이썬독학'])
-  const [따봉, 따봉변경] = useState(0)
+  const [따봉, 따봉변경] = useState([0, 0, 0])
   const [모달, 모달변경] = useState(false)
+  const [누른제목, 누른제목변경] = useState(0)
+  const [입력값, 입력값변경] = useState('')
   // const posts = '강남 고기 맛집'
 
-  function 모달바꾸기() {
-    모달변경(!모달)
+  function 글추가() {
+    글제목변경([입력값, ...글제목])
+    따봉변경([0, ...따봉])
   }
 
+  function 모달바꾸기(idx) {
+    if (idx === 누른제목) {
+      모달변경(!모달)
+    }
+    누른제목변경(idx)
+  }
+
+  function 따봉바꾸기(idx) {
+    const 뉴따봉 = [...따봉]
+    뉴따봉[idx] += 1
+    따봉변경(뉴따봉)
+  }
+
+  function 반복된UI() {
+    const 어레이 = []
+
+    for (let i = 0; i<3; i++) {
+      어레이.push(<div>안녕</div>)
+    }
+
+    return 어레이;
+  }
   // function 제목바꾸기() {
   //   var newArray = [...글제목]
   //   newArray[0] = '여자코트 추천' 
@@ -23,32 +48,34 @@ function App() {
       <div className='black-nav'>
         <div>개발 Blog</div>
       </div>
-      <div className='list'>
-        <h3> {글제목[0] } <span onClick={ () => { 따봉변경(따봉 + 1) } }>👍</span> { 따봉 }</h3>
-        <p>2월 17일 발행</p>
-        <hr/>
+      {
+        글제목.map((e, i) => {
+          return (
+            <div className='list' key={i}>
+              <h3 onClick={ () => { 모달바꾸기(i) } }>{ e }<span onClick={ () => { 따봉바꾸기(i) } }>👍</span> { 따봉[i] }</h3>
+              <p>2월 17일 발행</p>
+              <hr/>
+            </div>
+          )
+        })
+      }
+
+      <div className='publish'>
+        <input onChange={ (e) => 입력값변경(e.target.value)}/>
+        <button onClick={ 글추가 }>저장</button>
       </div>
-      <div className='list'>
-        <h3> {글제목[1] } <span onClick={ () => { 따봉변경(따봉 + 1) } }>👍</span> { 따봉 }</h3>
-        <p>2월 18일 발행</p>
-        <hr/>
-      </div>
-      <div className='list'>
-        <h3 onClick={ 모달바꾸기 }> { 글제목[2] }</h3>
-        <p>2월 19일 발행</p>
-        <hr/>
-      </div>
-      { 모달 ? <Modal /> : null }
+
+
+      { 모달 ? <Modal 글제목={ 글제목 } 누른제목={ 누른제목 } /> : null }
      
     </div>
     )
 }
 
-function Modal() {
+function Modal(props) {
   return (
-    
     <div className='modal'>
-      <h2>제목</h2>
+      <h2>제목: { props.글제목[props.누른제목] }</h2>
       <p>날짜</p>
       <p>상세내용</p>
     </div>
---
+++ shop/src/App.js
@@ -0,0 +1,68 @@
+/* eslint-disable */
+
+import { Button, Navbar, Container, Nav, NavDropdown } from 'react-bootstrap';
+import { useState } from 'react';
+import './App.css';
+import Data from './data.js' 
+
+function App() {
+
+  const [shoes, shoes변경] = useState(Data);
+  return (
+    <div className="App">
+      <Navbar bg="light" expand="lg">
+        <Container>
+          <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="#link">Link</Nav.Link>
+              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+
+      <div className='jumbotron'>
+        <h1>20% Season Off</h1>
+        <p>
+          This is a simple hero unit
+        </p>
+        <p>
+          <Button variant="primary">Learn more</Button>
+        </p>
+      </div>
+
+      <div className='container'>
+        <div className='row'>
+          {
+            shoes.map((v, i) => {
+              return (
+                <Item shoes={ shoes[i] } i={ i } key={i} />
+              )
+            })
+          }
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function Item(props) {
+  return (
+    <div className='col-md-4' id='product'>
+      <img src={ `https://codingapple1.github.io/shop/shoes${props.i+1}.jpg`} width="100%" />
+      <h4>{ props.shoes.title }</h4>
+      <p>{ props.shoes.content } & { props.shoes.price }</p>
+    </div>
+  )
+}
+
+export default App;
