ae05cae83e76ef8ef1a526a6676b2c8fa216325a
91hongppie <91hongppie@gmail.com>
2022-07-04 19:04:20 +0900

    TIL_React: restartShop

--- shop/src/App.js
+++ shop/src/App.js
@@ -1,67 +1,47 @@
-/* eslint-disable */
-
-import { Button, Navbar, Container, Nav, NavDropdown } from 'react-bootstrap';
-import { useState } from 'react';
 import './App.css';
-import Data from './data.js' 
+import { Button, Navbar, Container, Nav, Row, Col } from 'react-bootstrap'
+import { useState } from 'react';
+import { data, a } from './data'
 
 function App() {
 
-  const [shoes, shoes변경] = useState(Data);
+  let [shoes] = useState(data);
   return (
     <div className="App">
-      <Navbar bg="light" expand="lg">
+      <Navbar bg="dark" variant="dark">
         <Container>
-          <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto">
-              <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#link">Link</Nav.Link>
-              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-              </NavDropdown>
-            </Nav>
-          </Navbar.Collapse>
+          <Navbar.Brand href="#home">ShoeShop</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#pricing">Cart</Nav.Link>
+          </Nav>
         </Container>
+
       </Navbar>
+      <div className={'main-bg'}> </div>
 
-      <div className='jumbotron'>
-        <h1>20% Season Off</h1>
-        <p>
-          This is a simple hero unit
-        </p>
-        <p>
-          <Button variant="primary">Learn more</Button>
-        </p>
-      </div>
+      <div className={'container'}>
+        <div className={'row'}>
+            {shoes.map((value) => {
+              return <Card value={value} />
 
-      <div className='container'>
-        <div className='row'>
-          {
-            shoes.map((v, i) => {
-              return (
-                <Item shoes={ shoes[i] } i={ i } key={i} />
-              )
-            })
-          }
+            })}
+
+          </div>
         </div>
-      </div>
     </div>
   );
 }
 
-function Item(props) {
+function Card(props) {
+    const shoes = props.value
   return (
-    <div className='col-md-4' id='product'>
-      <img src={ `https://codingapple1.github.io/shop/shoes${props.i+1}.jpg`} width="100%" />
-      <h4>{ props.shoes.title }</h4>
-      <p>{ props.shoes.content } & { props.shoes.price }</p>
-    </div>
+      <div className={'col-md-4'}>
+        <img src={`https://codingapple1.github.io/shop/shoes${shoes.id + 1}.jpg`} width={'80%'}/>
+        <h4>{shoes.title}</h4>
+        <p>{shoes.content}</p>
+        <p>{shoes.price}</p>
+      </div>
   )
 }
 
